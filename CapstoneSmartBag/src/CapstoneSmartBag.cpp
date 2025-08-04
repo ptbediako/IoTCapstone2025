a2a0de35467d9f964286f39fc76196baa8f4a265
Phylicia Bediako <phylicia.bediako@gmail.com>
2025-08-04 16:50:02 -0600

    Capstone code draft

--- CapstoneSmartBag/src/CapstoneSmartBag.cpp
+++ CapstoneSmartBag/src/CapstoneSmartBag.cpp
@@ -8,9 +8,22 @@
 
 // Include Particle Device OS APIs
 #include "Particle.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1306.h"
+#include "IoTTimer.h"
+#include "credentials.h"
 
-const int WATERSENSOR=D5;
+//Water Sensor
+const int WATERSENSOR=D5; //is more responsive at 5V than 3.3V
 float waterVal;
+
+//OLED
+const int OLED_RESET=-1;
+Adafruit_SSD1306 display(OLED_RESET);
+const int OLEDADDR=0x3C;
+
+//Timer
+IoTTimer timer; 
 
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
@@ -19,11 +32,28 @@ void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected,10000);
 
+  display.begin(SSD1306_SWITCHCAPVCC,OLEDADDR);
+  display.display();
+
   pinMode(WATERSENSOR,INPUT);
+
+  timer.startTimer(10);
 }
 
 
 void loop() {
   waterVal=digitalRead(WATERSENSOR);
-  Serial.printf("%f\n",waterVal);
+  //Serial.printf("%f\n",waterVal);
+
+  // display.clearDisplay();
+  // display.setTextSize(1);
+  // display.setTextColor(WHITE);
+  // display.setCursor(0,0);
+  // display.clearDisplay();
+  // display.printf("");
+  // display.display();
+
+  //if(timer.isTimerReady()){
+    //  ;
+    //}
 }
