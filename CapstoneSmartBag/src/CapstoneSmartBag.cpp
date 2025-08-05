93997b786d9a863e58e01e36a102fab51607638c
Phylicia Bediako <phylicia.bediako@gmail.com>
2025-08-05 09:01:18 -0600

    Capstone code 08052025

--- CapstoneSmartBag/src/CapstoneSmartBag.cpp
+++ CapstoneSmartBag/src/CapstoneSmartBag.cpp
@@ -12,10 +12,36 @@
 #include "Adafruit_SSD1306.h"
 #include "IoTTimer.h"
 #include "credentials.h"
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT/Adafruit_MQTT_SPARK.h"
+#include "Adafruit_MQTT/Adafruit_MQTT.h"
+
+//PUBLISH code here
+//TCPClient TheClient;
+//Adafruit_MQTT_Publish inTemp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/iotcapstone.insidebagtemp");
+//Adafruit_MQTT_Publish leak = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/iotcapstone.leakindicator");
+//Adafruit_MQTT_Publish outTemp = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/iotcapstone.outsidebagtemp");
+
+float pubValue;
+void MQTT_connect();
+void MQTT_ping();
+unsigned int lastPubTime; //maybe for printing at regular intervals?
 
 //Water Sensor
 const int WATERSENSOR=D5; //is more responsive at 5V than 3.3V
-float waterVal;
+int waterVal;
+String waterMsg;
+
+//BME Variables--NEED 2 (ADDRESSES x76 AND x77)
+float inTempC,inTempF,outTempC,outTempF;
+const char DEGREE=0xF8;
+const char PCT=0x25;
+const int BME280=0x76;
+// const int BMEXXXX=0x77;
+bool status;
+Adafruit_BME280 bmeInner;
+//2ndBME bmeOuter;
+bool tempProblem, leakProblem;
 
 //OLED
 const int OLED_RESET=-1;
@@ -28,9 +54,27 @@ IoTTimer timer;
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
 
+/*******************************************************************/
 void setup() {
   Serial.begin(9600);
   waitFor(Serial.isConnected,10000);
+
+  WiFi.on();
+  WiFi.connect();
+  while(WiFi.connecting()) {
+    Serial.printf(".");
+  }
+  Serial.printf("\n\n");
+
+  status = bmeInner.begin(BME280);
+   if (status == false){
+     Serial.printf("BME280 at address 0x%02x failed to start",BME280);
+   }
+
+  //  status = bmeOuter.begin(XXXXXXX);
+  //  if (status == false){
+  //    Serial.printf("BMEXXXXX at address 0x%02x failed to start",BMEXXXXX);
+  //  }
 
   display.begin(SSD1306_SWITCHCAPVCC,OLEDADDR);
   display.display();
@@ -40,9 +84,36 @@ void setup() {
   timer.startTimer(10);
 }
 
-
+/********************************************************************/
 void loop() {
+  MQTT_connect();
+  MQTT_ping();
+  inTempC=bmeInner.readTemperature();
+  inTempF=(tempC*1.8)+32;
+
+  outTempC=bmeOuter.readTemperature();
+  outTempF=(tempC*1.8)+32;
+
   waterVal=digitalRead(WATERSENSOR);
+  if ((waterVal == 0)){
+    waterMsg = "No leak or spill detected";
+  }
+  else{
+    waterMsg = "Possible leak or spill";
+  }
+
+
+  if((millis()-lastPubTime)>30000){
+    if(mqtt.Update()){
+      hgSoilFeed.publish(soilDryness,1);
+      hgAirFeed.publish(airQuality,1);
+      hgTempFeed.publish(tempF,1);
+      hgHumidFeed.publish(humidRH,1);
+      hgPressFeed.publish(pressInHg,1);
+    }
+    lastPubTime = millis();
+  }
+
   //Serial.printf("%f\n",waterVal);
 
   // display.clearDisplay();
@@ -50,10 +121,47 @@ void loop() {
   // display.setTextColor(WHITE);
   // display.setCursor(0,0);
   // display.clearDisplay();
-  // display.printf("");
+  // display.printf("",inTempF,outTempF,waterVal);
   // display.display();
 
   //if(timer.isTimerReady()){
     //  ;
     //}
+}
+
+/*********************************************************************/
+void MQTT_connect() {
+  int8_t ret;
+ 
+  // Return if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+ 
+  Serial.print("Connecting to MQTT... ");
+ 
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.printf("Error Code %s\n",mqtt.connectErrorString(ret));
+       Serial.printf("Retrying MQTT connection in 5 seconds...\n");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds and try again
+  }
+  Serial.printf("MQTT Connected!\n");
+}
+
+/************************************************************/
+bool MQTT_ping() {
+  static unsigned int last;
+  bool pingStatus;
+
+  if ((millis()-last)>120000) {
+      Serial.printf("Pinging MQTT \n");
+      pingStatus = mqtt.ping();
+      if(!pingStatus) {
+        Serial.printf("Disconnecting \n");
+        mqtt.disconnect();
+      }
+      last = millis();
+  }
+  return pingStatus;
 }
